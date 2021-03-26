5fa8345a7350367ba1f2d03ec4ca541ab72ec398
Joerg Hilger <ride4sun@gmail.com>
2021-03-26 08:58:43 -0700

    init repository

---
+++ lib/super_enum.dart
@@ -0,0 +1,16 @@
+library super_enum;
+
+export 'dart:async';
+export 'package:meta/meta.dart';
+export 'package:equatable/equatable.dart' show Equatable;
+export 'src/annotations.dart'
+    show
+        superEnum,
+        generic,
+        Generic,
+        SuperEnum,
+        object,
+        ObjectClass,
+        DataField,
+        Data,
+        UseClass;
